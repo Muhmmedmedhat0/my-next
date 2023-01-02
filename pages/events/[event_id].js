8461f8db5b9dc39ccc85994d9bd0f08bbb2a89af
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2023-01-02 22:34:07 +0200

    working on event detail

--- pages/events/[event_id].js
+++ pages/events/[event_id].js
@@ -1,10 +1,29 @@
+import { useRouter } from "next/router";
 import React from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/events/event-detail/event-summary";
+import EventLogistics from "../../components/events/event-detail/event-logistics";
+import EventContent from "../../components/events/event-detail/event-content";
 
 function EventDetailPage() {
+  const router = useRouter();
+  const { event_id } = router.query;
+  const event = getEventById(event_id);
+
   return (
-    <div>
-      <h1>EventDetailPage</h1>
-    </div>
+    <>
+    {event ? <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.imageAlt}
+      />
+      <EventContent><p>{event.description}</p></EventContent>
+    </> : <h1 style={{textAlign:"center"}}>No Event Found!</h1>}
+      
+    </>
   );
 }
 
