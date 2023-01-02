0bbc0f1a4edc019ee3b99553d23495a2938c04fd
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2023-01-02 13:30:04 +0200

    setup project structure

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FilterdEventPage() {
+  return (
+    <div>
+      <h1>FilterdEventPage</h1>
+    </div>
+  );
+}
+
+export default FilterdEventPage;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 
 import styles from "../styles/Home.module.css";
 
@@ -7,36 +6,7 @@ export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-            <ul>
-              <li><Link href="/about">About</Link></li>
-              <li><Link href="/blog/1">Blog/1</Link></li>
-              <li>
-                <Link href="/products">Products</Link>
-                  <ul>
-                    <li><Link href="/products/1">products/1</Link></li>
-                  </ul>
-                  <ul>
-                    <li><Link href="/products/list">products/list</Link></li>
-                  </ul>
-              </li>
-              <li><Link href="/clients">Clients</Link>
-              <ul>
-              <li>
-                <Link href={`/clients/1`}>clients/:id</Link>
-                <ul>
-                  <li>
-                    <Link href="/clients/1/2">clients/:id/singleClintProject</Link>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <h1>Hello Next</h1>
       </main>
     </>
   );
