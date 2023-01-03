c6364f2f8a968aa2f631bd4d16d62535c4c778fa
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2023-01-03 16:15:24 +0200

    cleanup the full project

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { getFeaturedEvents } from "../dummy-data";
-import EventList from "../components/events/event-list";
+
 
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const featuredEvents = getFeaturedEvents();
+  
   return (
     <>
-      <EventList items={featuredEvents} />
+      <h1>Hello next</h1>
     </>
   );
 }
