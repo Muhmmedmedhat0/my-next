51045deb8eae9f843e2ad1ee7a1a116220bc9a67
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2023-01-05 21:21:47 +0200

    Combining Pre-Rendering With Client-Side Fetching

--- pages/last-sales/index.js
+++ pages/last-sales/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
-function LastSalesPage() {
-  const [sales, setSales] = useState();
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error, isLoading } = useSWR(
@@ -24,11 +24,8 @@ function LastSalesPage() {
     }
   }, [data]);
 
-  if (error) return <div className="center ">failed to load</div>;
-  if (!data || !sales || isLoading)
-    return <div className="center">loading...</div>;
-
-  // console.log(sales );
+  if (isLoading) return <div className="center">loading...</div>;
+  if (error || !data || !sales) return <div className="center ">failed to load</div>;
   return (
     <ul>
       <li>
@@ -41,6 +38,27 @@ function LastSalesPage() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-test-18425-default-rtdb.firebaseio.com/sales.json",
+  );
+  const data = await response.json();
+  const transformedData = [];
+
+  for (const key in data) {
+    transformedData.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return {
+    props: { sales: transformedData },
+    revalidate: 10,
+  };
 }
 
 export default LastSalesPage;
